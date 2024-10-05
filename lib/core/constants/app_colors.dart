d5c8f65dd283c6e44c52b40fb3fbfa8299787dd1
Les Holikov <ganyagolikov97@gmail.com>
2024-10-05 16:11:08 +0300

    Colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkBackground = Color(0xFF212121);
+  static const lightBackground = Color(0xFFFFFFFF);
+
+  static const primary = Color(0xFF3F51B5);
+  static const secondary = Color(0xFFFFC107);
+  static const white = Color(0xFFFFFFFF);
+
+  static const lightText = Color(0xFFFFFFFF);
+  static const lightSecondaryText = Color(0xFFA2A2A2);
+
+  static const darkText = Color(0xFF212121);
+  static const darkSecondaryText = Color(0xFF505050);
+
+  static const darkInputText = Color(0xFF505050);
+  static const lightInputText = Color(0xFFC1C1C1);
+}
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,117 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:stats_app/core/constants/app_colors.dart';
 
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
-      primaryColor: Colors.deepPurple,
-      scaffoldBackgroundColor: Colors.white,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.deepPurple,
-        primary: Colors.deepPurple,
-        secondary: Colors.amber,
-      ),
+      scaffoldBackgroundColor: AppColors.lightBackground,
       useMaterial3: true,
-      appBarTheme: const AppBarTheme(
-        color: Colors.transparent,
-        elevation: 0,
-        titleTextStyle: TextStyle(
-          color: Colors.white,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      textTheme: const TextTheme(
-        titleLarge: TextStyle(
-            fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black),
-        titleMedium: TextStyle(
-            fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
-        bodyLarge: TextStyle(fontSize: 16, color: Colors.black),
-        bodyMedium: TextStyle(fontSize: 14, color: Colors.grey),
-        labelMedium: TextStyle(
-            fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.deepPurple,
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          fixedSize: const Size(double.infinity, 48),
-        ),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: Colors.grey[200],
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide.none,
-        ),
-        hintStyle: TextStyle(color: Colors.grey[500]),
-      ),
-      sliderTheme: SliderThemeData(
-        activeTrackColor: Colors.deepPurple,
-        inactiveTrackColor: Colors.grey[300],
-        thumbColor: Colors.deepPurple,
-        overlayColor: Colors.deepPurple.withOpacity(0.2),
+      textTheme: _texts,
+      appBarTheme: _appBar,
+      colorScheme: _colorsScheme,
+      elevatedButtonTheme: _elevationButton,
+      inputDecorationTheme: _textInput.copyWith(
+        fillColor: AppColors.lightInputText,
       ),
     );
   }
 
   static ThemeData get dark {
     return ThemeData(
-      primaryColor: Colors.black,
-      scaffoldBackgroundColor: Colors.grey.shade900,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.deepPurple,
-        primary: Colors.deepPurple,
-        secondary: Colors.amber,
-      ),
+      scaffoldBackgroundColor: AppColors.darkBackground,
       useMaterial3: true,
-      appBarTheme: const AppBarTheme(
-        color: Colors.transparent,
-        elevation: 0,
-        titleTextStyle: TextStyle(
-          color: Colors.white,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      textTheme: const TextTheme(
-        headlineLarge: TextStyle(
-            fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white),
-        headlineMedium: TextStyle(
-            fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
-        bodyLarge: TextStyle(fontSize: 16, color: Colors.white),
-        bodyMedium: TextStyle(fontSize: 14, color: Colors.grey),
-        labelMedium: TextStyle(
-            fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.deepPurple,
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          minimumSize: const Size(double.infinity, 48),
-        ),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: Colors.grey[800],
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide.none,
-        ),
-        labelStyle: const TextStyle(color: Colors.white),
-      ),
-      sliderTheme: SliderThemeData(
-        activeTrackColor: Colors.deepPurple,
-        inactiveTrackColor: Colors.grey[600],
-        thumbColor: Colors.deepPurple,
-        overlayColor: Colors.deepPurple.withOpacity(0.2),
+      textTheme: _texts,
+      appBarTheme: _appBar,
+      colorScheme: _colorsScheme,
+      elevatedButtonTheme: _elevationButton,
+      inputDecorationTheme: _textInput.copyWith(
+        fillColor: AppColors.darkInputText,
       ),
     );
   }
+
+  static final _colorsScheme = ColorScheme.fromSeed(
+    seedColor: AppColors.primary,
+    primary: AppColors.primary,
+    secondary: AppColors.secondary,
+  );
+
+  static const _appBar = AppBarTheme(
+    color: Colors.transparent,
+    elevation: 0,
+  );
+
+  static const _texts = null;
+
+  static final _textInput = InputDecorationTheme(
+    filled: true,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide.none,
+    ),
+  );
+
+  static final _elevationButton = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: AppColors.white,
+      backgroundColor: AppColors.primary,
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      minimumSize: const Size(double.infinity, 48),
+    ),
+  );
 }
