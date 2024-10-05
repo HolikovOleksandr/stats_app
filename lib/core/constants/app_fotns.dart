e27bec0dc7e6194970c7b4a9b1ce1b683650744d
Les Holikov <ganyagolikov97@gmail.com>
2024-10-05 16:56:09 +0300

    Fonts

---
+++ lib/core/constants/app_fotns.dart
@@ -0,0 +1,34 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static final TextStyle titleL = GoogleFonts.montserrat(
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+  );
+
+  static final TextStyle titleM = GoogleFonts.montserrat(
+    fontWeight: FontWeight.bold,
+    fontSize: 22,
+  );
+
+  static final TextStyle titleS = GoogleFonts.montserrat(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+
+  static final TextStyle bodyL = GoogleFonts.montserrat(
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+  );
+
+  static final TextStyle bodyM = GoogleFonts.montserrat(
+    fontWeight: FontWeight.normal,
+    fontSize: 14,
+  );
+
+  static final TextStyle bodyS = GoogleFonts.montserrat(
+    fontWeight: FontWeight.normal,
+    fontSize: 12,
+  );
+}
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,50 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:stats_app/core/constants/app_colors.dart';
+import 'package:stats_app/core/constants/app_fotns.dart';
 
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
-      scaffoldBackgroundColor: AppColors.lightBackground,
       useMaterial3: true,
-      textTheme: _texts,
       appBarTheme: _appBar,
       colorScheme: _colorsScheme,
       elevatedButtonTheme: _elevationButton,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      textTheme: _buildTextTheme(AppColors.darkText),
       inputDecorationTheme: _textInput.copyWith(
-        fillColor: AppColors.lightInputText,
+        fillColor: const Color(0x89F2F2F2),
       ),
     );
   }
 
   static ThemeData get dark {
     return ThemeData(
-      scaffoldBackgroundColor: AppColors.darkBackground,
       useMaterial3: true,
-      textTheme: _texts,
       appBarTheme: _appBar,
       colorScheme: _colorsScheme,
       elevatedButtonTheme: _elevationButton,
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      textTheme: _buildTextTheme(AppColors.lightText),
       inputDecorationTheme: _textInput.copyWith(
         fillColor: AppColors.darkInputText,
       ),
     );
   }
 
-  static final _colorsScheme = ColorScheme.fromSeed(
-    seedColor: AppColors.primary,
-    primary: AppColors.primary,
-    secondary: AppColors.secondary,
-  );
-
-  static const _appBar = AppBarTheme(
-    color: Colors.transparent,
-    elevation: 0,
-  );
-
-  static const _texts = null;
+  static TextTheme _buildTextTheme(Color color) => TextTheme(
+        titleLarge: AppFonts.titleL.copyWith(color: color),
+        titleMedium: AppFonts.titleM.copyWith(color: color),
+        titleSmall: AppFonts.titleS.copyWith(color: color),
+        bodyLarge: AppFonts.bodyL.copyWith(color: color),
+        bodyMedium: AppFonts.bodyM.copyWith(color: color),
+        bodySmall: AppFonts.bodyS.copyWith(color: color),
+      );
 
   static final _textInput = InputDecorationTheme(
     filled: true,
+    labelStyle: AppFonts.bodyM,
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(8),
       borderSide: BorderSide.none,
@@ -55,9 +53,23 @@ class AppTheme {
     style: ElevatedButton.styleFrom(
       foregroundColor: AppColors.white,
       backgroundColor: AppColors.primary,
+      minimumSize: const Size(double.infinity, 52),
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      minimumSize: const Size(double.infinity, 48),
+      textStyle: AppFonts.bodyL.copyWith(fontWeight: FontWeight.bold),
     ),
+  );
+
+  static final _colorsScheme = ColorScheme.fromSeed(
+    seedColor: AppColors.primary,
+    primary: AppColors.primary,
+    secondary: AppColors.secondary,
+  );
+
+  static final _appBar = AppBarTheme(
+    centerTitle: true,
+    titleTextStyle: AppFonts.titleM,
+    color: Colors.transparent,
+    elevation: 0,
   );
 }
